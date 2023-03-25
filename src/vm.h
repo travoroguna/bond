8394eba9d2fa2f6725406824281182bc81a34f83
Travor Oguna Oneya <trevoroguna@gmail.com>
2023-03-25 13:52:40 +0300

    vm: refactor, vm only contains code related to the vm

--- src/vm.h
+++ src/vm.h
@@ -11,61 +11,13 @@
 #include <type_traits>
 #include <fmt/core.h>
 
+#include "code.h"
+#include "object.h"
+
 namespace bond {
-    enum class ObjectType {
-        Number, String, Bool, Nil, Object
-    };
-
-    enum class RuntimeError {
-        TypeError, Unimplemented, DivisionByZero
-    };
-
-
-    #define UNIMPLEMENTED return std::unexpected(RuntimeError::Unimplemented)
-    #define OBJ_RESULT std::expected<Object*, RuntimeError>
-
-    class Object {
-    public:
-        virtual OBJ_RESULT $add(Object const& other) { UNIMPLEMENTED; }
-        virtual OBJ_RESULT $sub(Object const& other) { UNIMPLEMENTED; }
-        virtual OBJ_RESULT $mul(Object const& other) { UNIMPLEMENTED; }
-        virtual OBJ_RESULT $div(Object const& other) { UNIMPLEMENTED; }
-        virtual ~Object() = default;
-
-        bool is_marked() { return m_marked; }
-        void mark() { m_marked=true; }
-        void unmark() { m_marked=false; }
-        ObjectType get_type() const { return m_type; }
-
-        Object* next {nullptr};
-
-    protected:
-        ObjectType m_type {ObjectType::Object};
-        bool m_marked { false };
-
-    };
-
-    class Number : public Object{
-    public:
-        explicit Number(float value) { m_value = value; m_type = ObjectType::Number; }
-        float get_value() const { return m_value; }
-        ~Number() override = default;
-
-        OBJ_RESULT $add(Object const& other) override;
-        OBJ_RESULT $sub(Object const& other) override;
-        OBJ_RESULT $mul(Object const& other) override;
-        OBJ_RESULT $div(Object const& other) override;
-    private:
-        float m_value {0};
-    };
-
     class Vm{
     public:
         Vm() = default;
-        void add_object(Object *obj);
-
-    private:
-        Object *head = nullptr;
     };
 
 } // bond
